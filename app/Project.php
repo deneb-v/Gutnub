9a62cbebf91474ed3c672d5143ac974c1e0fe6b1
deneb-v <53167397+deneb-v@users.noreply.github.com>
2020-12-13 14:35:14 +0700

    debug

--- app/Project.php
+++ app/Project.php
@@ -58,7 +58,8 @@ class Project extends Model
     }
 
     static public function getProjectUpdate($userID){
-        $data = DB::select('SELECT y.projectName, y.updated_at, y.name, y.fileName, y.description FROM (SELECT pm.projectID FROM users u JOIN project_members pm ON pm.userID = u.id where u.id = ?) as x, (SELECT p.projectID, p.projectName, f.updated_at, x.name , x.fileName, x.description FROM (SELECT a.id AS id, a.name AS Name, f.fileName, f.description, p.projectID FROM users a JOIN files f ON f.userID = a.id JOIN projects p ON p.projectID = f.projectID ) as x, users u JOIN project_members pm ON pm.userID = u.id JOIN projects p ON p.projectID = pm.projectID JOIN files f ON f.projectID = p.projectID WHERE x.id = u.id AND p.projectID = x.projectID GROUP BY x.fileName, p.projectName, x.name, p.projectID) as y WHERE x.projectID = y.projectID', [1, $userID]);
+        // $data = DB::select('SELECT y.projectName, y.updated_at, y.name, y.fileName, y.description FROM (SELECT pm.projectID FROM users u JOIN project_members pm ON pm.userID = u.id where u.id = ?) as x, (SELECT p.projectID, p.projectName, f.updated_at, x.name , x.fileName, x.description FROM (SELECT a.id AS id, a.name AS Name, f.fileName, f.description, p.projectID FROM users a JOIN files f ON f.userID = a.id JOIN projects p ON p.projectID = f.projectID ) as x, users u JOIN project_members pm ON pm.userID = u.id JOIN projects p ON p.projectID = pm.projectID JOIN files f ON f.projectID = p.projectID WHERE x.id = u.id AND p.projectID = x.projectID GROUP BY x.fileName, p.projectName, x.name, p.projectID) as y WHERE x.projectID = y.projectID', [1, $userID]);
+        $data = [];
         return $data;
     }
 }
