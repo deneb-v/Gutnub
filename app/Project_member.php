1a1daac709dfe94acc7f1344fab0094cf712268e
deneb-v <53167397+deneb-v@users.noreply.github.com>
2020-12-30 17:29:02 +0700

    pk null

--- app/Project_member.php
+++ app/Project_member.php
@@ -7,12 +7,16 @@ use Illuminate\Support\Facades\DB;
 
 class Project_member extends Model
 {
+
+    protected $primaryKey = null;
     protected $table='project_members';
     protected $fillable=[
         'projectID',
         'userID',
         'role'
     ];
+
+    public $incrementing = false;
 
     public function user(){
         return $this->belongsTo('App\User', 'userID');
