d8cb36fe0d7685870a1c8ec4e438989dbaaa9e22
deneb-v <53167397+deneb-v@users.noreply.github.com>
2020-12-30 17:02:49 +0700

    heroku config

--- config/database.php
+++ config/database.php
@@ -21,8 +21,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
-    // 'default' => 'pgsql',
+    // 'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => 'pgsql',
 
     /*
     |--------------------------------------------------------------------------
