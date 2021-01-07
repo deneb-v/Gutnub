c57bf3131c04ceedca7f3de72af56f026aeb8399
deneb-v <53167397+deneb-v@users.noreply.github.com>
2021-01-07 09:08:52 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -70,16 +70,16 @@ return [
             ]) : [],
         ],
 
-        // 'pgsql' => [
-        //     'driver'   => 'pgsql',
-        //     'host'     => $host,
-        //     'database' => $database,
-        //     'username' => $username,
-        //     'password' => $password,
-        //     'charset'  => 'utf8',
-        //     'prefix'   => '',
-        //     'schema'   => 'public',
-        // ],
+        'pgsql' => [
+            'driver'   => 'pgsql',
+            'host'     => parse_url(env('DATABASE_URL', ""))['host'],
+            'database' => substr(parse_url(env('DATABASE_URL', ""))['path'], 1),
+            'username' => parse_url(env('DATABASE_URL', ""))['user'],
+            'password' => parse_url(env('DATABASE_URL', ""))['pass'],
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+        ],
 
 
         'sqlsrv' => [
